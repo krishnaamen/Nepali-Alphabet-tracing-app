efd05dca0d1a4c0f9a2747b2af3a43081ff35338
krishnaamen <krishnaamen@gmail.com>
2019-11-10 08:36:40 +0100

    first commit
    first commit before fixing adds

---
+++ Alphabet/Assets/Alphabet Board/Scripts/Admanager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleMobileAds.Api;
+
+public class Admanager : MonoBehaviour
+{
+    public static Admanager instance;
+    private string appID = "ca-app-pub-3940256099942544~3347511713";
+    private BannerView bannerView;
+    private string bannerID = "ca-app-pub-3940256099942544/6300978111";
+    private InterstitialAd fullScreenAd;
+    private string fullScreenAdID;
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        } else
+        {
+            Destroy(this);
+        }
+    }
+    public void RequestBanner()
+    {
+        bannerView = new BannerView(bannerID, AdSize.Banner, AdPosition.Bottom);
+        AdRequest request = new AdRequest.Builder().Build();
+        bannerView.LoadAd(request);
+        bannerView.Show();
+
+    }
+}
---
+++ Alphabet/Assets/MainMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+   public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        //Admanager.instance.RequestBanner();
+    }
+    public void QuitGame()
+    {
+        Debug.Log("I am Quiting.");
+        Application.Quit();
+    }
+  public void backToSetting()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+       // Admanager.instance.RequestBanner();
+    }
+}
